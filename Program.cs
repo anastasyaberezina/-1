811be87a34dc4d99930f93cc1d536842ab5f74d1
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-20 01:53:12 +0300

    попытка написания проги 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Программа, принимающая на вход 2 числа, и проверяющая, является ли первое число квадратом второго.
+
+Console.Write("Введите первое число: ");
+
+int number1 = int.Parse(Console.ReadLine({number1}));
+
+Console.Write("Введите второе число: ");
+
+int number2 = int.Parse(Console.ReadLine({number2}));
+
+int sqr = number2 * number2;
+
+if (number1 = sqr)
+{
+ Console.WriteLine($"{number1} является квадратом {number2}");
+}
+if (number1 > sqr)
+{
+ Console.WriteLine($"{number1} НЕ является квадратом {number2}");
+}
+if (number1 < sqr)
+{
+ Console.WriteLine($"{number1} НЕ является квадратом {number2}");
+}
+else 
+{
+ Console.WriteLine($"{number1} НЕ является квадратом {number2}");
+}
+
+Console.WriteLine($"{number1} НЕ является квадратом {number2}");
